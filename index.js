1f30cea360b23a6942348a26d9f7a5b3c1dc9179
hkjang <gagagiga@naver.com>
2021-03-18 14:57:08 +0900

    update index

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 const axios = require('axios')
+
 const jeus = {
   admin: {
     command (url, auth, params) {
       return axios.post(url, params, {
+        auth: auth
+      });
+    },
+    api (url, auth, params) {
+      let commands = {
+        "jeusadmin": params
+      }
+      return axios.post(url, commands, {
         auth: auth
       });
     }
