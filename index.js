69eb352c29a21c623aa2d439d6a5d7c7a83db613
hkjang <gagagiga@naver.com>
2021-03-18 10:45:05 +0900

    update index

--- index.js
+++ index.js
@@ -1,20 +1,10 @@
 const axios = require('axios')
 const jeus = {
   admin: {
-    getServerInfo (url, auth, params) {
+    command (url, auth, params) {
       return axios.post(url, params, {
         auth: auth
-      }).then(function(response) {
-        console.log('Authenticated');
-      }).catch(function(error) {
-        console.log('Error on Authentication');
       });
-    }
-  },
-  monitor: {
-    /**  API */
-    attach (auth, options = {}) {
-      return Request.post(auth, '/post/attach', options)
     }
   }
 }
